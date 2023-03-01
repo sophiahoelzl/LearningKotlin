c5e1e364160b4e6fc3301ecd69a6ab1eddce4db7
sophiahoelzl <sophia.hoelzl@stud.fh-campuswien.ac.at>
2023-03-01 15:23:37 +0100

    all done

--- src/main/kotlin/4NumberGame.kt
+++ src/main/kotlin/4NumberGame.kt
@@ -4,57 +4,110 @@ fun main() {
     game()
 }
 
-var guessedNum = ""
-var userInput = ""
+var userInput = ""  //String that hold user input
+var h = HashSet<Int>(4) //HashSet for random number (Hashset because easier with duplicates)
+
+var check: Boolean = false //boolean to check if game is over
 
 
-fun random() {
-    val size = 4
+fun random(): Array<Int> {  //random number generator, returns Array<Int> (easier for me to compare)
 
-    val s = HashSet<Int>(size)
-    while (s.size < size){
-        s += Random.nextInt(0,9)
+    while (h.size < 4){
+        h += Random.nextInt(0,9)
     }
 
-    for (i in s){
-        val n = i.toString()
-        guessedNum += n
-    }
-
+    return h.toTypedArray()
 }
 
 
+fun UserInput(): IntArray { //Method for User input
+    userInput = readLine()!! //same as nextLine() in java
 
-fun UserInput(){
-    userInput = readLine()!!
-
-    if (userInput.length > 4 || userInput.contains("[A-z]".toRegex())){
+    if (userInput.length != 4 || userInput.contains("[A-z]".toRegex())){    //to check if only 4 numbers are entered
         println("Only 4 digits are allowed!!")
         UserInput()
     }else {
         println("Your guess is: $userInput")
     }
-}
 
-fun compare(userInput: String, guessedNum: String) {
+    var user = IntArray(4)
 
-    if (userInput.equals(guessedNum)){
-        println("Congrats! You guessed it correctly!!")
+    var e = 0
+    for (i in userInput){
+        user[e] = i.digitToInt() //put user input also in Array
+        e++
     }
+
+    return user
 }
 
+var rightNumWrongPlace = 0 //Right digit but wrong place
+var rightNumRightPlace = 0 //Right digit and right place
+
+fun compare(userInput: IntArray, guessedNum: IntArray): Boolean { //copmares the two arrays, returns boolean if number was guessed
+
+    for (a in userInput){ // checks the right digit but wrong place
+        for (b in guessedNum){
+            if (a.equals(b)){
+                rightNumWrongPlace++
+            }
+        }
+    }
+
+    for (i in 0..3){    // checks the right digit and right place
+        if (userInput[i] == guessedNum[i]){
+            rightNumRightPlace++
+
+        }
+    }
+
+
+    if (rightNumRightPlace == 4){ //checks if number was guessed correct if rightNumRightPlace is 4
+        return true
+    }
+
+    return false
+}
 
 fun game() {
 
-    random()
-    println("Random 4-digit number was generated! Now we can start the game!")
-    println("Enter 4 digits to guess the number:")
-    UserInput()
+    var a = IntArray(4)     //
+    var w = 0                    //
+                                 //
+    var arr = random()           //
+                                 //     initializes the generated random number
+    for (i in arr){              //
+        a[w] = i                 //
+        w++                      //
+    }                            //
 
-    compare(userInput, guessedNum)
+    /*
+    for (i in h){
+        print(i)
+    }*/
 
-    println("You guessed this correctly: ")
+    println()
+    println("Random 4-digit number was generated! Now we can start the game:)!")
+    println()
 
+    while (check == false){     // checks if the number was already guessed
+
+        println("*************************************")
+        println("Enter 4 digits to guess the number:")
+        var user = UserInput()
+
+        check = compare(user, a)
+
+        println("You guessed this correctly: $rightNumWrongPlace : $rightNumRightPlace ")
+        println()
+
+        rightNumWrongPlace = 0
+        rightNumRightPlace = 0
+    }
+
+    println("=================================================")
+    println("   Congrats!! You guessed the number correct:)   ")
+    println("=================================================")
 
 }
 
