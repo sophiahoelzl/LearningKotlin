831fa3fc9c3077de3f74547e6534476eaf24ee5a
sophiahoelzl <sophia.hoelzl@stud.fh-campuswien.ac.at>
2023-03-01 11:11:30 +0100

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,19 @@
+
+
+fun main() {
+    println("sophia for the win")
+
+    var number1: Int = 5
+    number1 = 10
+
+    val number2: Int = 6
+    //number2 = 9
+
+    var str1: String? = "hello" //declared with ?
+    str1 = null                 //no error
+
+    var str2: String = "hello"  //declared without ?
+    //str2 = null                 //compile error
+
+}
+
